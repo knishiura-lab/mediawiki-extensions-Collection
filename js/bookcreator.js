28cffc24795b1c046bb4ae47e675cfc6648b08f2
daisy1754 <d-part@hotmail.co.jp>
2014-02-06 14:14:03 +0900

    Swap arguments.

--- js/bookcreator.js
+++ js/bookcreator.js
@@ -52,7 +52,7 @@ $(function() {
 			if (args.length == 3) {
 				oldid = args[2];
 			}
-			refreshBookCreatorBox(hint, oldid);
+			refreshBookCreatorBox(oldid, hint);
 			save_collection(result.collection);
 		}, 'json');
 	}
